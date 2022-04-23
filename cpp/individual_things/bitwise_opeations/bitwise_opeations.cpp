7311317687dac2c053951e2db3cd93b79e6ad73e
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-04-23 17:23:12 +0300

    add cVcpp_bitwise_operators

--- cpp/individual_things/bitwise_opeations/bitwise_opeations.cpp
+++ cpp/individual_things/bitwise_opeations/bitwise_opeations.cpp
@@ -1,12 +1,12 @@
 // bitwise_opeations
 #pragma region Includes
 #include <iostream>
+#include <bitset>
 #pragma endregion Includes
 
 int main(int argc, char const *argv[])
 {
     int a = 5;
-    // int b = a<<1;
     std::cout << "a = " << a << "\n";
     std::cout << "a << 1 = " << (a << 1) << "\n";
     std::cout << "a << 4 = " << (a << 4) << "\n";
@@ -18,5 +18,11 @@ int main(int argc, char const *argv[])
               << "\n";
     std::cout << "y >> x = ( -y2^x )"
               << "\n";
+
+    a = 0b101;        // binary 101 = 5
+    int b = 0b010110; // binary 010110 = 22
+    b <<= 4; // binary 010110 << 4 = 0101100000 = 352
+    int c = a | b;
+    std::cout << std::bitset<9>(c) << "\n";
     return 0;
 }
