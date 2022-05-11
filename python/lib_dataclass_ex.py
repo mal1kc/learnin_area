baa5f1f49964a52b7514a4eab7e7c8656ca25a8d
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-05-12 00:31:29 +0300

    added some python examples

--- python/lib_dataclass_ex.py
+++ python/lib_dataclass_ex.py
@@ -1,2 +1,57 @@
+'''
+example of python dataclass decorator and property method
+'''
 
-# to be edited
+from dataclasses import dataclass
+
+# dataclass is for creating class with data attributes
+# for creating dataclass we use @dataclass decorator and it adds automatically __init__ and __repr__ methods for our class
+
+
+@dataclass  # decorator for dataclass
+class Book:
+    title: str  # str type data attribute of Book class
+    author: str  # str type data attribute of Book class
+    pages: int  # int type data attribute of Book class
+    price: float = 2.99  # float type data attribute of Book class with default value 2.99
+    quantity: int = 1  # int type data attribute of Book class with default value 1
+
+    # bool type data attribute of Book class with default value True
+    is_available: bool = True
+
+    # def total_price(self):
+    #     return self.price * self.quantity
+
+    # for creating property for total price
+    total_price = property(lambda self: self.price * self.quantity)
+
+    def set_book_info(self, value):
+        # raise exception for setter book_info method
+        raise Exception("Book info cannot be set")
+
+    def get_book_info(self) -> dict[str, object]:
+        return {'title': self.title, 'author': self.author, 'pages': self.pages,
+                'price': self.price, 'quantity': self.quantity, 'is_available': self.is_available}
+    # dict data type attribute of Book class with only getter method
+
+    # created book_info property for Book class with property function and used getter and setter methods of book_info property that we created earlier
+    book_info = property(get_book_info, set_book_info,
+                         doc='info about the book object')
+
+
+def main() -> None:
+    book1 = Book('Dune', 'Frank Herbert', 432, 5.99, 2,
+                 True)  # creating object from Book class
+    print('data of book1: \n', book1.book_info,end='\n\n')
+
+    book2 = Book('Martian', 'Andy Weir', 200, 2.99,
+                 50, False)  # create book2 object
+    print('data of book2: \n', book2.book_info,end='\n\n')
+
+    book3 = Book('I, Robot', 'Isaac Asimov', 200,
+                 2.99, 3, False)  # create book3 object
+    print('data of book3: \n', book3.book_info,end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
--- python/lib_loggging_ex.py
+++ python/lib_loggging_ex.py
@@ -1,2 +1,55 @@
+'''
+python logging library usage example
+'''
+import logging
 
-# to be edited
+
+# * priority levels:
+# - DEBUG - lowest level
+# - INFO - default level
+# - WARNING - level for warning messages
+# - ERROR - level for error messages
+# - CRITICAL - highest level
+# * for more info about logging library see: https://docs.python.org/3/library/logging.html
+
+logging.basicConfig(level=logging.DEBUG, filename='logfile.log',
+                    filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+
+# - prints message in logfile.log with levelname DEBUG
+logging.debug('This is a debug message')
+# - prints message in logfile.log with levelname INFO
+logging.info('This is an info message')
+# - prints message in logfile.log with levelname WARNING
+logging.warning('This is a warning message')
+# - prints message in logfile.log with levelname ERROR
+logging.error('This is an error message')
+# - prints message in logfile.log with levelname CRITICAL
+logging.critical('This is a critical message')
+
+variable = 'test'
+# - prints message in logfile.log with levelname DEBUG
+logging.debug('the value of variable is: %s', variable)
+print('the value of variable is: %s' % variable)
+
+try:
+    1/0
+except Exception as e:
+    logging.exception('exception occurred: %s' % e)
+
+# * custom loggers
+
+logger = logging.getLogger('my_logger')  # create logger with name my_logger
+
+# create file handler for logger
+handler = logging.FileHandler(f'{logger.name}.log')
+
+# create formatter for logger
+formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s - %(lineno)d ')
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+# prints message in logfile.log with levelname INFO
+logger.info('this is an info message')
+
+# logging.info('program ended')
