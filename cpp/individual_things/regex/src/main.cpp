b54d317c2783ca00f0d154075c45bd6ebd93644c
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-10-08 18:47:26 +0300

    added cpp regex example

--- cpp/individual_things/regex/src/main.cpp
+++ cpp/individual_things/regex/src/main.cpp
@@ -1,57 +1,87 @@
+#include <ios>
 #include <iostream>
 #include <regex>
-#include <string>
 
-// int main(int argc, char *argv[]) {
-//   std::string example_text = "MemTotal:       16313192 kB"
-//                              "MemFree:        10332752 kB"
-//                              "MemAvailable:   13959848 kB";
-//   std::regex regexp("([0-9]+\\S\\w)");
-//   std::smatch m;
-//   if (std::regex_search(example_text, m, regexp)) {
-//     std::cout << "string that matches the pattern:"
-//               << "\n";
-//     // for (auto x : m)
-//     std::cout << m.size() << "\n";
-//     for (uint i = 0; i < m.size(); i++)
-//       std::cout << m[i] << " ";
-//   }
-//   return 0;
-// }
+// source of example code  ---|
+//                            
+// "https://www.fluentcpp.com/2020/02/28/c-regex-101-simple-code-for-simple-cases-with-regexes/"
+
+/* NOTE std::regex used for creating regex patterns
+
+      std::regex regexp = std::regex(R"(\d+)");
+*/
+/* NOTE std::regex_match is used for check if the entire searched text matches
+ * \ the pattern of regex
+ *
+ * NOTE std::regex_search is used for check if searched text contains a sub-part
+ * \ that matches the pattern of regex
+ *
+ * NOTE in other way,
+ *
+ *  std::regex_match is used to validated that string follows a pattern
+ * \ (a date, a phone number, and so on)
+ *
+ *  std::search is used to perform the equivalent of a greo in piece of text
+ */
+
+int example_regexlib_usage() {
+  auto const regex = std::regex(R"((my|your) regex)");
+  auto const myText = std::string("A piece of text that contains my regex.");
+  bool myTextContainsRegex =
+      std::regex_search(myText, regex); // searching for regex match
+
+  // auto const yourText =
+  //     std::string("A piece of text that contains your regex.");
+  // bool const yourTextContainsRegex = std::regex_search(yourText, regex);
+
+  // auto const theirText =
+  //     std::string("A piece of text that contains their regex.");
+  // bool const theirTextContainsRegex = std::regex_search(theirText, regex);
+
+  // std::cout << std::boolalpha << myTextContainsRegex << '\n'
+  //           << yourTextContainsRegex << '\n'
+  //           << theirTextContainsRegex << '\n';
+  auto matchResults = std::smatch{};
+  myTextContainsRegex = std::regex_search(
+      myText, matchResults, regex); // for finding position of match
+
+  std::cout << "text : " << myText << "\n"
+            << "regex pattern : "
+            << "((my|your) regex)"
+            << "\n"
+            << "text have any matches : " << std::boolalpha
+            << myTextContainsRegex << "\n";
+  std::cout << "prefix : " << matchResults.prefix() << "\t, suffix"
+            << matchResults.suffix() << "\n";
+  std::cout << "matchResults location is : " << matchResults.prefix().length()
+            << "," << matchResults.length() << "\nmatch : ["
+            << myText.substr(matchResults.prefix().length(),
+                             matchResults.length())
+            << "]\n\n";
+  return 0;
+}
+
+int my_regex_example() {
+
+  using namespace std;
+
+  auto const regexp = regex(R"(\d+)");
+  auto const example_text = string("MemTotal:       16313192 kB"
+                                   "MemFree:        10332752 kB"
+                                   "MemAvailable:   13959848 kB");
+  bool const example_contains_regex = regex_search(example_text, regexp);
+  cout << boolalpha << example_contains_regex << "\n";
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
-  using namespace std;
-  string sp("geeks for geeks"
-            "geks for geeks"
-            "geeks for geeks");
-  regex re("(\\w+)");
 
-  // flag type for determining the matching behavior
-  // && here it is for matches on strings.
-  smatch match;
+  std::cout << "working example output"
+            << "\n";
+  example_regexlib_usage();
+  std::cout << "my example output"
+            << "\n";
 
-  // we can use member function on match
-  // to extract the matched pattern.
-  if (regex_match(sp.begin(), sp.end(), match, re) == true) {
-
-    // The size() member function indicates the
-    // number of capturing groups plus one for the overall match
-    // match size = Number of capturing group + 1
-    // (.*) which "forgeeks" ).
-    cout << "Match size = " << match.size() << endl;
-
-    // Capturing group is index from 0 to match_size -1
-    // .....here 0 to 2
-    // pattern at index 0 is the overall match "geeksforgeeks"
-    // pattern at index 1 is the first capturing group "geeks"
-    // pattern at index 2 is the 2nd capturing group "forgeeks"
-
-    cout << "Whole match : " << match.str(0) << endl;
-    cout << "First capturing group is '" << match.str(1)
-         << "' which is captured at index " << match.position(1) << endl;
-  } else {
-    cout << "No match is found" << endl;
-  }
-  return 0;
+  my_regex_example();
   return 0;
 }
